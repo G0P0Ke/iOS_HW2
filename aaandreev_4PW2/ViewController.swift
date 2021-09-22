915e5a550bc0b8f956b88b057fb5b0338248f470
Andreev Anton <boy.of.future@mail.ru>
2021-09-22 21:54:13 +0300

    1 Point: The project has a settings button, as described below.

--- aaandreev_4PW2/ViewController.swift
+++ aaandreev_4PW2/ViewController.swift
@@ -11,9 +11,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColorFromHex(rgbValue: 0x80ffff)
+        setupSettingsButton()
         // Do any additional setup after loading the view.
     }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    private func setupSettingsButton(){
+        let settingsButton = UIButton(type: .system)
+        view.addSubview(settingsButton)
+        settingsButton.setImage(
+            UIImage(named: "settings")?.withRenderingMode(.alwaysOriginal),
+            for: .normal
+        )
+        settingsButton.translatesAutoresizingMaskIntoConstraints = false
+        settingsButton.topAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.topAnchor,
+            constant: 15
+        ).isActive = true
+        settingsButton.trailingAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+            constant: -15
+        ).isActive = true
+        settingsButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        settingsButton.widthAnchor.constraint(equalTo: settingsButton.heightAnchor).isActive = true
+        
+    }
 
+    func UIColorFromHex(rgbValue:UInt32,_ alpha:Double=1.0) -> UIColor {
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
 
+        return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
+    }
 }
 
