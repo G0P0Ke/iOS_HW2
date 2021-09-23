c4c09d61b4ded1d81aef5da9c2fead58a9037133
Andreev Anton <boy.of.future@mail.ru>
2021-09-23 17:41:38 +0300

    2 Points: Pressing settings button makes settings view to appear/disappear

--- aaandreev_4PW2/ViewController.swift
+++ aaandreev_4PW2/ViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let settingsView = UIView()
+    let settingsButton = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColorFromHex(rgbValue: 0x80ffff)
         setupSettingsButton()
+        setupSettingsView()
         // Do any additional setup after loading the view.
     }
     
@@ -20,9 +23,22 @@ class ViewController: UIViewController {
         return .lightContent
     }
     
+    @objc
+    private func settingsButtonPressed() {
+        UIView.animate(
+            withDuration: 0.1,
+            animations: {
+                self.settingsView.alpha = 1 - self.settingsView.alpha
+        })
+    }
+    
     private func setupSettingsButton(){
-        let settingsButton = UIButton(type: .system)
         view.addSubview(settingsButton)
+        settingsButton.addTarget(
+            self,
+            action: #selector(settingsButtonPressed),
+            for: .touchUpInside
+        )
         settingsButton.setImage(
             UIImage(named: "settings")?.withRenderingMode(.alwaysOriginal),
             for: .normal
@@ -40,7 +56,24 @@ class ViewController: UIViewController {
         settingsButton.widthAnchor.constraint(equalTo: settingsButton.heightAnchor).isActive = true
         
     }
-
+    
+    private func setupSettingsView(){
+        settingsView.backgroundColor = UIColorFromHex(rgbValue: 0xe0ffff)
+        view.addSubview(settingsView)
+        settingsView.translatesAutoresizingMaskIntoConstraints = false
+        settingsView.topAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.topAnchor,
+            constant: 45
+        ).isActive = true
+        settingsView.trailingAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+            constant: -45
+        ).isActive = true
+        settingsView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        settingsView.widthAnchor.constraint(equalToConstant:  200).isActive = true
+        
+    }
+    
     func UIColorFromHex(rgbValue:UInt32,_ alpha:Double=1.0) -> UIColor {
         let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
         let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
